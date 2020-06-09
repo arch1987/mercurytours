71138816ca450eec23e67159d27013b88abc0031
arch1987 <agajula028@gmail.com>
2020-06-09 11:08:47 +0530

    adding changed run.sh and test.sh files

--- test.sh
+++ test.sh
@@ -11,7 +11,6 @@ export TARGET_PATH="/kaarten.html"
 export TARGET_KEYWORD="Kaartdiensten"
 
 T_DIR=tests/trivial
-
 # Reporting dir: start fresh
 R_DIR=${T_DIR}/report
 rm -rf ${R_DIR} > /dev/null 2>&1
@@ -19,10 +18,8 @@ mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
-./run.sh -Dlog_level.jmeter=DEBUG \
-	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
-	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
-	-n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
+./run.sh $TARGET_PATH -Dlog_level.jmeter=DEBUG \ -JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
+	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \ -n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
