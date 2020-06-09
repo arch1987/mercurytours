8887d42f264143da903510062fbd9d3cab911c61
arch1987 <agajula028@gmail.com>
2020-06-09 12:04:55 +0530

    adding small change to test.sh

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
-./run.sh $TARGET_PATH -Dlog_level.jmeter=DEBUG \ -JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
+./run.sh -Dlog_level.jmeter=DEBUG \ -JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
 	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \ -n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
