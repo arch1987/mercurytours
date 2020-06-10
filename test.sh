c12bf9965f40f1415ebcaec38b90fedbd9409908
arch1987 <agajula028@gmail.com>
2020-06-10 15:01:38 +0530

    made change in tesh.sh file adding file path

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@
 export TARGET_HOST="www.amazon.in"
 export TARGET_PATH="/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
 
-T_DIR=tests/amazon
+T_DIR=C:/Users/admin/IdeaProjects/git.repos/amazon.project/tests/amazon
 # Reporting dir: start fresh
 R_DIR=${T_DIR}/report
 rm -rf ${R_DIR} > /dev/null 2>&1
