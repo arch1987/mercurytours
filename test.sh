63562aadc3c358b2724578b9aa7745909fa625e8
arch1987 <agajula028@gmail.com>
2020-06-10 14:41:45 +0530

    commiting amazon project

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Test the JMeter Docker image using a trivial test plan.
+
+# Example for using User Defined Variables with JMeter
+# These will be substituted in JMX test script
+# See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
+export TARGET_HOST="www.amazon.in"
+export TARGET_PATH="/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
+
+T_DIR=tests/amazon
+# Reporting dir: start fresh
+R_DIR=${T_DIR}/report
+rm -rf ${R_DIR} > /dev/null 2>&1
+mkdir -p ${R_DIR}
+
+/bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
+
+./run.sh $TARGET_PATH -Dlog_level.jmeter=DEBUG \ -JTARGET_HOST=${TARGET_HOST} \
+	-JTARGET_PATH=${TARGET_PATH} \ -n -t ${T_DIR}/Test_Plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
+	-e -o ${R_DIR}
+
+echo "==== jmeter.log ===="
+cat ${T_DIR}/jmeter.log
+
+echo "==== Raw Test Report ===="
+cat ${T_DIR}/test-plan.jtl
+
+echo "==== HTML Test Report ===="
+echo "See HTML test report in ${R_DIR}/index.html"
