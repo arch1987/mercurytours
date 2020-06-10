4b2fb370356b4e74fc32343b53d0b56ca3941a57
arch1987 <agajula028@gmail.com>
2020-06-10 22:30:33 +0530

    modified test.sh

--- test.sh
+++ test.sh
@@ -7,10 +7,9 @@
 # See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
 export TARGET_HOST="amazon.in"
 #export TARGET_PATH="/ap/signin"
-T_DIR=tests/amazon
+T_DIR=tests
 # Reporting dir: start fresh
-R_DIR=C:amazon.project\report
-mkdir -p ${R_DIR}
+R_DIR=report
 
-./run.sh -JMeterTARGET_HOST=${TARGET_HOST} -n -t ${T_DIR}/Test_Plan.jmx -l ${R_DIR}/amazonresults_1.jtl
+./run.sh -JMeterTARGET_HOST=${TARGET_HOST} -n -t ${T_DIR}/Test_Plan.jmx -l ${T_DIR}/amazonresults_1.jtl
 
